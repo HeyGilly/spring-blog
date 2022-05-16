e6caf03c4f3aa2a8f48e834221ec74aa6b2f025f
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-05-16 09:26:55 -0500

    Working with partials

---
+++ src/main/java/com/codeup/springblog/controllers/PartialsController.java
@@ -0,0 +1,18 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/partials")
+public class PartialsController {
+
+    // This is going to redirect us to the partials-test.
+    @GetMapping
+    public String showPartialsTest(){
+        return "views-lecture/partials-test";
+    }
+
+
+}
