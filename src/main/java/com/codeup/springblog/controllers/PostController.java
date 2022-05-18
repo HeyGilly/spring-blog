d92e12265a5352be8affe69a24fa3be020053e56
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-05-18 10:31:48 -0500

    Updated the old code with the new information for the exercise, was missing a constructor and the form in the wrong action. posts -> post

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -21,40 +21,13 @@ public class PostController {
     }
 
 
-    public List<Post> generatePosts(){
-        List<Post> allPosts = new ArrayList<>();
-        Post post1 = new Post(1, "First Post", "This is going to be the body");
-        Post post2 = new Post(2, "Second Post", "This is going to be the body");
-        Post post3 = new Post(3, "Third Post", "This is going to be the body");
-        allPosts.add(post1);
-        allPosts.add(post2);
-        allPosts.add(post3);
-        return allPosts;
-    }
-
-
-
     // going to redirect you to the index for this post page.
     @GetMapping
     public String allPosts(Model model) {
-            List<Post> allPosts = generatePosts();
+            List<Post> allPosts = postsDao.findAll();
             model.addAttribute("allPosts", allPosts);
         return "post/index";
     }
-
-    @GetMapping("/{id}")
-    public String onePost(@PathVariable long id, Model model){
-        List<Post> allPosts = generatePosts();
-        Post post = null;
-        for (int i = 0; i<allPosts.size(); i++){
-            if(allPosts.get(i).getId() == id){
-                post = allPosts.get(i);
-            }
-        }
-        model.addAttribute("post", post);
-        return "post/show";
-    }
-
 
     @GetMapping("/create")
     public String createPost(){
@@ -62,8 +35,41 @@ public class PostController {
 }
 
     @PostMapping("/create")
-    @ResponseBody
-    public String submitPost(){return "You just created a post";}
+    // action formwas incorrect
+    // no constructor
+
+    public String submitPost(@RequestParam(name = "title") String title, @RequestParam(name = "body") String body){
+        Post post = new Post(title, body);
+        postsDao.save(post);
+        return "redirect:/post";
+    }
+
+////---------- OLD Material
+
+    //    public List<Post> generatePosts(){
+//        List<Post> allPosts = new ArrayList<>();
+//        Post post1 = new Post(1, "First Post", "This is going to be the body");
+//        Post post2 = new Post(2, "Second Post", "This is going to be the body");
+//        Post post3 = new Post(3, "Third Post", "This is going to be the body");
+//        allPosts.add(post1);
+//        allPosts.add(post2);
+//        allPosts.add(post3);
+//        return allPosts;
+//    }
+
+    //    @GetMapping("/{id}")
+//    public String onePost(@PathVariable long id, Model model){
+//        List<Post> allPosts = generatePosts();
+//        Post post = null;
+//        for (int i = 0; i<allPosts.size(); i++){
+//            if(allPosts.get(i).getId() == id){
+//                post = allPosts.get(i);
+//            }
+//        }
+//        model.addAttribute("post", post);
+//        return "post/show";
+//    }
+
 
 
 }
